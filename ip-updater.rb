26781b785db06d3a51ad00ac4b2781c0e7c78bbc
Ricardo Martins <>
2007-05-17 20:39:08 +0100

    Updated dynamic dns updater

--- ip-updater.rb
+++ ip-updater.rb
@@ -1,16 +1,17 @@
 #!/usr/bin/env ruby
 #
-#  EveryDNS updater
+#  EveryDNS and FreeDNS updater
 #
 #  Created by Ricardo Martins on 2007-02-06.
 #  Copyright (c) 2007. All rights reserved.
 
-require 'open-uri'
-require 'net/http'
-require 'uri'
-require 'base64'
+%w[open-uri net/http uri base64].each {|x| require x}
 
-ip = open('http://www-i4.informatik.rwth-aachen.de/~lexi/whatsmyip.php').read.strip
+# Which service do you want to update?
+everydns = false
+freedns = true
+
+# EveryDNS login details
 username = 'user'
 password = 'password'
 domain = 'domain.org'
@@ -19,19 +20,22 @@ version = '0.1'
 pass = Base64.encode64("#{username}:#{password}")
 url = "/index.php?ver=#{version}&ip=#{ip}&domain=#{domain}"
 
-old_ip = begin; File.open('/root/.myip').read.strip; rescue; ""; end
+# FreeDNS url update link
+fdns = 'http://freedns.afraid.org/dynamic/update.php?supermegaspecialsecretlinkzor'
+
+ip = open('http://www-i4.informatik.rwth-aachen.de/~lexi/whatsmyip.php').read.strip
+old_ip = begin; File.open(ENV['HOME'] + '/.myip').read.strip; rescue; ""; end
 
 if ip != old_ip
   # Update EveryDNS
-  begin
+  if everydns
     h = Net::HTTP.new(target, '80')
     h.get(url, {'Authorization' => "Basic #{pass}"})
-  rescue # Don't die if this times out
   end
 
   # Update FreeDNS
-  open('http://freedns.afraid.org/dynamic/update.php?zomgsupersecretlinkzor').read.strip
+  open(fdns).read.strip if freedns
 
   # Update recorded ip
-  File.open('/root/.myip', 'w+') {|f| f << ip}
+  File.open(ENV['HOME'] + '/.myip', 'w+') {|f| f << ip}
 end
