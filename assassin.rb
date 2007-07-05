8c33a2d670bd7b8e3440f3b44dd6d05b92d9a0e0
Ricardo Martins <>
2007-07-05 11:59:50 +0100

    Simplified signature assassin a lot

--- assassin.rb
+++ assassin.rb
@@ -7,28 +7,10 @@
 
 MAIL = open(ARGV[0]).read
 @mail = Array.new
-@sig_found = nil
-@blank = false
 
 MAIL.each_line do |line|
-  unless @sig_found
-    if not line =~ /^>\s\s*(--(\s*|\w*)$|__)/
-      if line =~ /^>\s$/
-        @mail << line unless @blank
-        @blank = true
-      else
-        @mail << line
-        @blank = false
-      end
-    else
-      @sig_found = true
-    end
-  end
-end
-
-# Add signature file, if found
-if File.exists?(ENV['HOME'] + '/.signature') and not @mail.last == open(ENV['HOME'] + '/.signature').read
-  @mail << "\n--\n" + open(ENV['HOME'] + '/.signature').read
+  break if line =~ />\s(__|--)/
+  @mail << line
 end
 
 File.open(ARGV[0], 'w+') {|file| @mail.each {|line| file << line}}
