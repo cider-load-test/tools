0bf0b270e438943e3788d49a10bcdd86e8c7f44f
Ricardo Martins <>
2007-09-20 14:21:08 +0100

    Added IRC renamer

---
+++ rename.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+#
+#  Simple renamer. Removes peer name from files downloaded through
+#  IRC with weechat.
+#
+#  (C)opyright 2007 Ricardo Martins <meqif at scarybox dot net>
+#  Licensed under the MIT/X11 License. See LICENSE file for license details.
+
+require 'ftools'
+
+files = ARGV
+peers = %w{ HardGay Mirrors Neverwhere [UR]DAN Nippon|zongzing Lancre}
+
+files.each do |file|
+  name = (file.split('.') - peers).join('.')
+  File.mv(file, name)
+end
