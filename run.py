905d0469502a23e1f9d6323c1ce379cd14342551
Ricardo Martins <>
2007-10-12 22:01:48 +0100

    Smallish cleaning up of run.py

--- run.py
+++ run.py
@@ -4,35 +4,10 @@
 # run.py - simple launch/run program written in Python
 # by Ricardo Martins <meqif@scarybox.net>
 #
-# Why did I write this? For 3 reasons:
-# 1. I was sick of having to have xfce4 installed just because of xfrun4,
-# since I run Openbox
-# 2. My Python skills were getting rusty
-# 3. I wanted to learn a bit more
-#
-# It is rather minimalist because I wanted it to look like ion3 launch:
-# simple and nice. Minimalistic stuff.
-#
 # A big thanks to nightm4re (dave foster <daf@minuslab.net>), since my history
 # code was inspired in his screen launch script for OB3 (screenscript.pl).
 #
-# I know the code isn't perfect nor efficient, but it gets the job done.
-# I'm open to suggestions, criticisms, improvements...
-#
 # Licensed under the BSD License
-#
-# 0.0.1 - 19/12/2005 - Initial release
-#
-# 0.0.2 - 20/12/2005 - The window opens always at the top left corner of
-# the screen, kde-based programs now launch, quits if loses focus (useful in
-# case you lose the window :P) -- All of these were [Knuckles]'s
-# suggestions. Thanks pal!
-#
-# 0.0.3 - 22/12/2005 - Cleaned up most debug prints
-#
-# 0.0.4 - 23/12/2005 - Some more cleaning, replacing pointless prints
-# with pass where no action is needed
-#
 #
 ## TODO:
 #	- Cleanup and optimize the code
@@ -46,26 +21,26 @@ import os, gtk
 program = ""
 command_id = 0
 
-## location of the command history
+# location of the command history
 history_file = os.environ.get('HOME')+"/.rprhist"
 
-## get our command history
+# get our command history
 history = open(history_file, "a+").readlines()
-## add a last blank command and reverse the list
+# add a last blank command and reverse the list
 history.append("")
 history.reverse()
 
-## add used commands to history
+# add used commands to history
 def history_add(program):
-	## the program is NOT in history
+	# the program is NOT in history
 	if (program+"\n") not in history:
 		history.append(program+"\n")
 		open(history_file, "w+").writelines(history)
-	## the program IS in the history
+	# the program IS in the history
 	else:
 		pass # Goggles -- Turn: Do nothing
 
-## go back in history
+# go back in history
 def history_back():
 	global command_id
 	if command_id < len(history)-1:
@@ -74,37 +49,34 @@ def history_back():
 	else:
 		pass # there's no older command in history
 	
-## advance in history
+# advance in history
 def history_next():
 	global command_id
 	if command_id > 0:
 		command_id -= 1
-		## here we set the entry text to the next command. stripping "\n"
-		## is necessary, otherwise the selected program won't run
+		# here we set the entry text to the next command. stripping "\n"
+		# is necessary, otherwise the selected program won't run
 		entry.set_text(history[command_id].strip("\n"))
 	else:
 		pass # there's no newer command in history
 
-## Properties of the window
+# Properties of the window
 window = gtk.Window()
 window.set_title("Run program")
 window.set_decorated(False)
 window.set_border_width(0)
 window.move(0,0)
 
-## launch the program
+# launch the program
 def on_entry_activated(entry):
 	program = entry.get_text()
 	history_add(program)
 	#os.spawnlp(os.P_NOWAIT, program)
-	## ARGH. Ugly hack here, because KDE-based programs
-	## screw up when launched through os.spawn*:
-	## ERROR: KUniqueApplication: Registering failed!
-	## ERROR: Communication problem with k3b, it probably crashed.
+  # Ugly hack here. There must be a cleaner way to do this.
 	os.execlp(program, ">/dev/null 2>&1 &")
 	gtk.main_quit()
 
-## what to do if keys are pressed
+# what to do if keys are pressed
 def key_press_impl(win,event):
 	if event.keyval == gtk.keysyms.Escape:
 		gtk.main_quit()
@@ -115,11 +87,11 @@ def key_press_impl(win,event):
 	elif event.keyval == gtk.keysyms.Tab:
 		pass ## will use this for tab-completion
 
-## exit if the entry get unfocused
-## handy if people lose the window or something
+# exit if the entry get unfocused
 def focus_out(object,event):
 	gtk.main_quit()
 
+# Create the windows
 vbox = gtk.VBox(False, 0)
 window.add(vbox)
 vbox.show()
@@ -131,7 +103,5 @@ entry.connect("focus_out_event", focus_out)
 entry.set_text("")
 vbox.pack_start(entry, True, True, 0)
 entry.show()
-
-## And now, the hardest part! :P
 window.show_all()
 gtk.main()
